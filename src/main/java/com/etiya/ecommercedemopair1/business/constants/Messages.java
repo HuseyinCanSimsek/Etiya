7619fe25d8cea30d2c8f32baf85f2a1175a38ed7
HuseyinCanSimsek <can.simsek@etiya.com>
2022-12-08 10:17:10 +0300

    All Magic Strings Replaced

---
+++ src/main/java/com/etiya/ecommercedemopair1/business/constants/Messages.java
@@ -0,0 +1,21 @@
+package com.etiya.ecommercedemopair1.business.constants;
+
+public class Messages {
+    public static class User
+    {
+       public  static final  String userExists="This user doesn't exist";
+    }
+    public static class City
+    {
+       public static final String cityExists="This city doesn't exist";
+    }
+    public static class Country
+    {
+        public static final String countryExists="This country doesn't exist";
+    }
+    public static class Category
+    {
+        public static final String categoryNameExists="This category already exists";
+        public static final String categoryExistsInProduct="This category doesn't exist. Could not be added product";
+    }
+}
---
+++ src/main/java/com/etiya/ecommercedemopair1/business/constants/Paths.java
@@ -0,0 +1,5 @@
+package com.etiya.ecommercedemopair1.business.constants;
+
+public class Paths {
+    public static final String apiPrefix="/api/v3/";
+}
